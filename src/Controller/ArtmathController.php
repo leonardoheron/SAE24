580fd93ea55260305c77c26c3fc906183b2fdc6e
Leonardo Heron <leonardo.heron@etu.univ-st-etienne.fr>
2022-05-24 21:46:57 +0200

    commit

--- src/Controller/ArtmathController.php
+++ src/Controller/ArtmathController.php
@@ -37,7 +37,7 @@ class ArtmathController extends AbstractController
     {
         // Redirige vers /artmath si on va sur le site sans
         //  indiquer le nom de la route
-        return $this->redirectToRoute('app_artmath');
+        return $this->render('artmath/home.html.twig');
     }
 
     /**
@@ -47,6 +47,14 @@ class ArtmathController extends AbstractController
     {
         return $this->render('artmath/index.html.twig', [
             'fichier' => '',
+        ]);
+    }
+    /**
+     * @Route("/fig2", name="app_fig2")
+     */
+    public function fig2(): Response
+    {
+        return $this->render('artmath/fig2.html.twig', [
             'fichier_nees' => '',
         ]);
     }
@@ -58,25 +66,14 @@ class ArtmathController extends AbstractController
     {
         // Récupère les paramètres issus du formulaire (on indique le champ name)
         $dimension = $request -> request -> get("dimension") ;
-        $dimension_ama = $request -> request -> get("dimension_ama") ;
-        $dimension_amr = $request -> request -> get("dimension_amr") ;
-        $dimension_nbc = $request -> request -> get("dimension_nbc") ;
-        $dimension_nbl = $request -> request -> get("dimension_nbl") ;
         // Pour les boutons : si appui contenu champ value sinon NULL
         $calculer  = $request -> request -> get("calculer");
         $imprimer  = $request -> request -> get("imprimer");
-        $calculer_nees  = $request -> request -> get("calculer_nees");
-        $imprimer_nees  = $request -> request -> get("imprimer_nees");
-
-
         // Oui : Appelle le script Python koch.py qui se trouve dans le répertoire /public
         $process = new Process(['python3','koch.py',$dimension]);
         $process -> run();
-        $process_nees = new Process(['python3',"nees_carre.py",$dimension_ama,$dimension_amr,$dimension_nbc,$dimension_nbl]);
-        $process_nees -> run();
         // Récupère la valeur de retour renvoyé par le script python
         $fichier=$process->getOutput();
-        $fichier_nees="reponse.png";
 
         // Retourne un message si l'éxécution c'est mal passée
         if (!$process->isSuccessful())
@@ -86,17 +83,47 @@ class ArtmathController extends AbstractController
         if ($calculer!=NULL)
             return $this->render('artmath/index.html.twig', [
                 'fichier' => $fichier,
-                'fichier_nees' => $fichier_nees,
-            ]);
-        if ($calculer_nees!=NULL)
-            return $this->render('artmath/index.html.twig', [
-                'fichier' => $fichier,
-                'fichier_nees' => $fichier_nees,
             ]);
         else {
             // On a appuyé sur imprimer
             return $this->render('artmath/imprimer.html.twig', [
                 'fichier' => $fichier,
+            ]);
+        }
+    }
+    /**
+     * @Route("/calculer_nees", name="calculer_nees")
+     */
+    public function calculer_nees(Request $request): Response
+    {
+        // Récupère les paramètres issus du formulaire (on indique le champ name)
+        $dimension_ama = $request -> request -> get("dimension_ama") ;
+        $dimension_amr = $request -> request -> get("dimension_amr") ;
+        $dimension_nbc = $request -> request -> get("dimension_nbc") ;
+        $dimension_nbl = $request -> request -> get("dimension_nbl") ;
+        // Pour les boutons : si appui contenu champ value sinon NULL
+        $calculer_nees  = $request -> request -> get("calculer_nees");
+        $imprimer_nees  = $request -> request -> get("imprimer_nees");
+
+
+        // Oui : Appelle le script Python koch.py qui se trouve dans le répertoire /public
+        $process_nees = new Process(['python3',"nees_carre.py",$dimension_ama,$dimension_amr,$dimension_nbc,$dimension_nbl]);
+        $process_nees -> run();
+        // Récupère la valeur de retour renvoyé par le script python
+        $fichier_nees="reponse.png";
+
+        // Retourne un message si l'éxécution c'est mal passée
+        if (!$process_nees->isSuccessful())
+            return new Response ("Erreur lors de l'éxécution du script Python :<br>".$process_nees->getErrorOutput());    
+
+        // A t'on appuyé sur calculer ?
+        if ($calculer_nees!=NULL)
+            return $this->render('artmath/fig2.html.twig', [
+                'fichier_nees' => $fichier_nees,
+            ]);
+        else {
+            // On a appuyé sur imprimer
+            return $this->render('artmath/imprimer_nees.html.twig', [
                 'fichier_nees' => $fichier_nees,
             ]);
         }
